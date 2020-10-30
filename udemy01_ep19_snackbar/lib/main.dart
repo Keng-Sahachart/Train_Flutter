50855b5b339516db8507399fedf6c668582691d4
Sahachart Panlawan <55072247+Keng-Sahachart@users.noreply.github.com>
2020-10-31 06:50:17 +0700

    ep19 SnackBar Complete
    continue ep19 SnackBar to Complete.

--- udemy01_ep19_snackbar/lib/main.dart
+++ udemy01_ep19_snackbar/lib/main.dart
@@ -8,7 +8,40 @@ class SnackBarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SnackBar Demo',
+        title: 'SnackBar Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("ScackBar Demo Test"),
+          ),
+          body: SnackBarPage(), /* ไปเรียก class มาแสดง*/
+        ));
+  }
+}
+
+class SnackBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      /* ไม่ได้เรียก MaterialApp */
+
+      child: RaisedButton(
+        onPressed: () {
+          /*ตัวแปร SnackBar พร้อมระบุ คำสั่ง label action*/
+          final snackBar = SnackBar(
+              content: Text('This is SnackBar'),
+              action: SnackBarAction( // ต้องประกาศ เป็น class Action ภายใน ตัว  SnackBar ด้วย
+                label: 'Undo',
+                onPressed: () {  // statement
+                  print('---->   printed from SnackBar   <----');
+                },
+              ));
+
+          /*หลังจาก ประกาศ SnackBar แล้ว มันยังทำงานเองไม่ได้ ต้องสั่งให้ show ด้วย ผ่าน context*/
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+        child: Text("Button"),
+        color: Colors.yellowAccent,
+      ),
     );
   }
 }
