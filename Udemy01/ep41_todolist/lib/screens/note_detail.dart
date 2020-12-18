2c009fdf1ef9e5850212cb31d2b7c64b5a8549f1
Sahachart Panlawan <55072247+Keng-Sahachart@users.noreply.github.com>
2020-12-18 08:50:43 +0700

    ep53

--- Udemy01/ep41_todolist/lib/screens/note_detail.dart
+++ Udemy01/ep41_todolist/lib/screens/note_detail.dart
@@ -7,16 +7,16 @@ class NoteDetail extends StatefulWidget {
   final String appBarTitle;
   final Note note;
 
-  NoteDetail(this.note,this.appBarTitle);
+  NoteDetail(this.note, this.appBarTitle);
 
   @override
-  State<StatefulWidget> createState(){
-    return NoteDetailState(this.note,this.appBarTitle);
+  State<StatefulWidget> createState() {
+    return NoteDetailState(this.note, this.appBarTitle);
   }
 }
 
 class NoteDetailState extends State<NoteDetail> {
-  static var priorities = ['High','Low'];
+  static var _priorities = ['High', 'Low'];
   DatabaseHelper helper = DatabaseHelper();
 
   String appBarTitle;
@@ -25,7 +25,7 @@ class NoteDetailState extends State<NoteDetail> {
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
 
-  NoteDetailState(this.note,this.appBarTitle);
+  NoteDetailState(this.note, this.appBarTitle);
 
   @override
   Widget build(BuildContext context) {
@@ -34,24 +34,70 @@ class NoteDetailState extends State<NoteDetail> {
     titleController.text = note.title;
     descriptionController.text = note.description;
 
-    return WillPopScope(onWillPop: (){
-      moveToLastScreen();
-    },child: Scaffold(
-      appBar: AppBar(
-        title: Text(appBarTitle),
-        leading: IconButton(
-          icon:Icon(Icons.arrow_back),
-          onPressed: (){
-            moveToLastScreen();
-          },
-        ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.only(top:15.0,left: 10.0,right: 10.0),
-        child: ListView(
+    return WillPopScope(
+        onWillPop: () {
+          moveToLastScreen();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(appBarTitle),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                moveToLastScreen();
+              },
+            ),
+          ),
+          body: Padding(
+            padding: EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+            child: ListView(
+              children: <Widget>[
+                /*  first element */
+                ListTile(
+                  title: DropdownButton(
+                    items: _priorities.map((String dropDownStringItem){
+                      return DropdownMenuItem<String>(
+                         value: dropDownStringItem,
+                          child: Text(dropDownStringItem)
+                      );
+                    }).toList(),
+                    style: textStyle,
+                    value: getPriorityAsString(note.priority),
+                    onChanged: (valueSelectedByUser){
+                      setState(() {
+                        debugPrint('User selected $valueSelectedByUser');
+                        updatePriorityAsInt(valueSelectedByUser);
+                      });
+                    },
+                  ),
+                ),
 
-        ),
-      ),
-    ) );
+                /* Second  */
+                Padding(
+                  padding: EdgeInsets.only(top:15.0, bottom: 15.0),
+                  child: TextField(
+                    controller: titleController,
+                    style: textStyle,
+                    onChanged: (value){
+                      debugPrint('Something change in Title Text Field');
+                      updateTitle();
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                      labelStyle: textStyle,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                      )
+                    ),
+                  ),
+
+                )
+
+                /* Third */
+
+              ],
+            ),
+          ),
+        ));
   }
 }
